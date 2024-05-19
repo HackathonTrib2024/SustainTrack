cfd26471f50f6035819d7deb714937980522d78f
Samad <samadnk00@gmail.com>
2024-05-19 13:08:07 -0700

    Initial commit

--- app/src/main/java/com/sustain/track/MainActivity.kt
+++ app/src/main/java/com/sustain/track/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
