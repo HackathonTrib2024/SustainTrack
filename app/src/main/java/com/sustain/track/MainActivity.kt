2ea10e6c9ddbe23d6f6029482aa8bb7e54a2b45a
Jovin <jovinpj@gmail.com>
2024-06-06 11:28:10 +0200

    * removed FAB and moved notification trigger logic to 'Pay or transfer from account' button

--- app/src/main/java/com/sustain/track/MainActivity.kt
+++ app/src/main/java/com/sustain/track/MainActivity.kt
@@ -8,11 +8,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -22,14 +18,12 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sustain.track.ui.navigation.BottomNavItem
 import com.sustain.track.ui.navigation.bottomNavList
-import com.sustain.track.ui.notification.NotificationUtils
 import com.sustain.track.ui.screens.TopBar
 import com.sustain.track.ui.screens.home.NewTransactionsHome
 import com.sustain.track.ui.screens.track.NewTrackScreen
@@ -43,9 +37,8 @@ class MainActivity : ComponentActivity() {
             SustainTrackTheme {
                 val navController = rememberNavController()
                 var selectedItemState by rememberSaveable { mutableIntStateOf(0) }
-                val context = LocalContext.current
                 Scaffold(
-                    topBar = {  TopBar(title = "SustainTrack") },
+                    topBar = { TopBar(title = "SustainTrack") },
                     bottomBar = {
                         NavigationBar {
                             bottomNavList.forEachIndexed { index, navItem ->
@@ -72,24 +65,6 @@ class MainActivity : ComponentActivity() {
                                 })
                             }
                         }
-                    },
-                    floatingActionButton = {
-                        ExtendedFloatingActionButton(
-                            text = { Text(text = "Pay") },
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Default.AddCircle,
-                                    contentDescription = "Pay"
-                                )
-                            },
-                            onClick = {
-                                NotificationUtils.showNotification(
-                                    context,
-                                    "750kr paid to Uber", "Your a/c XX0423 is debited for Kr.750 " +
-                                            "and credited to a/c XX6902(Ref no 4582604274045)." +
-                                            "Plz call 18002356464 if not done by you."
-                                )
-                            })
                     },
                 ) { innerPadding ->
                     Box(
--- app/src/main/java/com/sustain/track/ui/screens/home/NewTransactionHome.kt
+++ app/src/main/java/com/sustain/track/ui/screens/home/NewTransactionHome.kt
@@ -6,21 +6,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -29,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sustain.track.R
+import com.sustain.track.ui.notification.NotificationUtils
 import com.sustain.track.ui.screens.TransactionItemScreen
 
 
@@ -87,9 +85,16 @@ fun NewTransactionsHome(
                 color = Color(0xFF512B2B)
             )
         }
-
+        val context = LocalContext.current
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                NotificationUtils.showNotification(
+                    context,
+                    "750kr paid to Uber", "Your a/c XX0423 is debited for Kr.750 " +
+                            "and credited to a/c XX6902(Ref no 4582604274045)." +
+                            "Plz call 18002356464 if not done by you."
+                )
+            },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 16.dp),
